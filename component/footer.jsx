a906cf61f42bc12e02bc13041e128924c0313fe1
olasunkanmi <olasunkanmiologun64@email.com>
2024-04-24 11:16:32 +0100

    edit footer

--- component/footer.jsx
+++ component/footer.jsx
@@ -6,8 +6,7 @@ const Footer = () => {
 
     return (
         <footer>
-            <p>Design with <FaHandHoldingHeart className="heart" /> by <a href="mailto:olasunkanmiologun64@gmail.com"> <span>Ola</span>sunkanmi</a></p>
-            <p>&copy; <span>{currentYear}</span> Olasunkanmi github repo. All rights reserved</p>
+            <p>&copy; <span>{currentYear}</span> Olasunkanmi github repo. All rights reserved.</p>
         </footer>
     )
 }
